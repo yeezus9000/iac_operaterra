2a1162f1613feabecd2ac5ed7e85a316864e6dc2
yeezus9000 <github@estlan.net>
2024-11-13 14:28:47 +0100

    testing

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,11 +1,13 @@
 # Define the Azure SQL Server
 resource "azurerm_mssql_server" "sql_server" {
-  name                         = "${var.name_prefix}-sqlserver"
-  location                     = var.location
-  resource_group_name          = var.resource_group_name
-  administrator_login          = var.admin_username
-  administrator_login_password = var.admin_password
-  version                      = var.sql_version
+  name                = "${var.name_prefix}-sqlserver"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  # administrator_login           = var.admin_username
+  # administrator_login_password  = var.admin_password
+  public_network_access_enabled = var.public_network_access_enabled
+  version                       = var.sql_version
+
 }
 
 # Define the Azure SQL Database
