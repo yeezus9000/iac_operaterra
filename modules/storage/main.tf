f8385319a72cdc1618dfd79cdb4995a3c45c617d
yeezus9000 <github@estlan.net>
2024-11-13 15:31:56 +0100

    hjelp meg da

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -6,7 +6,7 @@ resource "random_string" "storage_suffix" {
 }
 locals {
   blob_name = "${var.name_prefix}_blob"
-  sa_name   = regexreplace("${var.name_prefix}${random_string.storage_suffix.result}", "[^a-zA-Z0-9]", "")
+  sa_name   = replace(lower("${var.name_prefix}${random_string.storage_suffix.result}"), "-", "")
 }
 
 # Define the Storage Account
@@ -18,20 +18,17 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = var.replication_type
 }
 
-
 # Define the Blob Container
 resource "azurerm_storage_container" "blob_container" {
   name                  = var.container_name
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_name  = local.sa_name
   container_access_type = var.container_access_type
 
 }
 
-
-
 resource "azurerm_storage_blob" "example" {
   name                   = local.blob_name
-  storage_account_name   = azurerm_storage_account.sa.name
-  storage_container_name = azurerm_storage_container.blob_container
+  storage_account_name   = local.sa_name
+  storage_container_name = var.container_name
   type                   = "Block"
 }
